fab881f4a7f838a3b31967a3e590998825135334
luwang <wanglu1207>
2021-12-09 09:34:14 -0800

    first commit

---
+++ app/src/main/java/com/example/tab/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.tab;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
